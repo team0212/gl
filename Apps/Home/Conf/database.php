ef11b0b52aecdd9f24105ff523d3de398e1eaefd
team0212 <team0212@users.noreply.github.com>
2024-03-14 18:44:38 +0000

    Initial release

---
+++ Apps/Home/Conf/database.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE' => 'mysql',// 数据库类型
+	'DB_HOST' => '47.115.42.111',// 服务器地址 192.168.44.200
+	'DB_NAME' => 'gold_lock',// 数据库名
+	'DB_USER' => 'root1',// 用户名
+	'DB_PWD' => 'czce.com.cn;5670',// 密码
+	'DB_PORT' => '3306',// 端口
+	'DB_PREFIX' => '',// 数据库表前缀
+	'DB_CHARSET' => 'utf8',// 字符集
+);
